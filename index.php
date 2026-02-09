f9c91e75fa39abdb915085b11d905b5c95e9f96e
sricharan12-hub <sric6618@gmail.com>
2026-02-09 23:21:29 +0530

    learned concatentaion

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ echo 25/10;
 ?>
 
 <?php 
-//checking trur or false
+//checking true or false
 $num1 = 23;
 $num2 = 35;
 echo "<br>";
@@ -29,6 +29,7 @@ var_dump($num1>$num2);
 ?>
 
 <?php 
+//loops
 echo "<br>";
 for ($i=0; $i<=5; $i++){
     
@@ -44,6 +45,7 @@ while ($i<=2){
 ?>
 
 <?php 
+//array and accessing array elements
 $fruits = ["mango",1,"Apple",2];
 echo "<br>";
 echo $fruits[0];
@@ -53,6 +55,32 @@ foreach($fruits as $fruit){
     echo $fruit;
 }
 ?>
+
+<?php 
+$var = null;
+echo "<br>";
+// using . we can concatinate strings
+$name = "Charan";
+echo "Hello, " . $name;
+// we can access a variable inside only double quotes not in single quotes
+echo "<br>";
+echo "Hello, $name";
+echo "<br>";
+echo 'Hi, $name';
+
+?>
+
+<!-- <?php -->
+// $value = trim(fgets(STDIN));
+// stdin used to take user input
+// if we run this code i am facing an error like how to give input 
+// echo "<br>";
+// echo $value;
+// ?>
+
+
+
+
 
 
 
